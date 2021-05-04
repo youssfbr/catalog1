56c1230723f88cebf24e65a5c1e18ed77266a561
Alisson Youssf <youssfbr@gmail.com>
2021-05-04 18:10:41 -0300

    Delete Category

--- backend/src/main/java/com/github/youssfbr/catalog/services/CategoryService.java
+++ backend/src/main/java/com/github/youssfbr/catalog/services/CategoryService.java
@@ -6,12 +6,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.github.youssfbr.catalog.dto.CategoryDTO;
 import com.github.youssfbr.catalog.entities.Category;
 import com.github.youssfbr.catalog.repositories.CategoryRepository;
+import com.github.youssfbr.catalog.services.exceptions.DatabaseException;
 import com.github.youssfbr.catalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -63,5 +66,17 @@ public class CategoryService {
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id " + id + " não encontrado!");
 		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} 
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id " + id + " não encontrado!");
+		} 
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Um recurso está associado a outro recurso! Não é possível deletar!");
+		}		
 	}
 }
